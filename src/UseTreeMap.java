6d444a52578f8b5338e1782aa323ce6a5f662502
Kira <datdhvinh@gmail.com>
2020-05-14 14:22:55 +0700

    Đếm số lần xuất hiện của mỗi từ trong một chuỗi sử dụng Map

---
+++ src/UseTreeMap.java
@@ -0,0 +1,19 @@
+import java.util.TreeMap;
+public class UseTreeMap {
+    public static void main(String[] args) {
+        String str = "Java is a programming language and computing platform first released by Sun Microsystems in 1995";
+        TreeMap<String, Integer> myTreeMap = new TreeMap();
+
+        String[] myString = str.split("\\s");
+
+        for (int i = 0; i < myString.length; i++) {
+            String myWord = String.valueOf(myString[i]);
+            if (myTreeMap.containsKey(myWord)) {
+                myTreeMap.put(myWord, myTreeMap.get(myWord) + 1);
+            } else {
+                myTreeMap.put(myWord, 1);
+            }
+        }
+        System.out.printf(String.valueOf(myTreeMap));
+    }
+}
